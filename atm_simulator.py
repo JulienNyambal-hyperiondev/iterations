254c738aee9986b20c8dde5918a5c4fe47621078
Julien Nyambal <julienn@hyperiondev.com>
2024-09-08 16:35:40 +0200

    most valuable product (MVP) for our atm toy

---
+++ atm_simulator.py
@@ -0,0 +1,26 @@
+balance = 1000
+max_attempts = 3
+
+current_attempt_number = 0
+while current_attempt_number < max_attempts:
+    withdrawal_amount = input("Enter the amount to be withdrawn: ")
+    withdrawal_amount_int = int(withdrawal_amount)
+    if withdrawal_amount_int <= balance and withdrawal_amount_int > 0:
+        balance = balance - withdrawal_amount_int
+        print(f"The new balance is {balance}")
+        break
+    else:
+        print("You cannot withdraw the money: Insufficient funds")
+    current_attempt_number = current_attempt_number + 1
+print("We are out of the loop")
+ 
+
+
+
+
+
+
+
+
+
+
